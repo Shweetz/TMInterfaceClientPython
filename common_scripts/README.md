8524f7910c22965512bbb18dd0933eec51c6e668
Romain Lambert <naruto92250@gmail.com>
2022-02-03 19:59:56 +0100

    Common scripts

---
+++ common_scripts/README.md
@@ -0,0 +1,30 @@
+# Common scripts - How to use
+## 1. bf_precise_finish.py
+Deactivate TMI's bruteforce 
+
+Open the script with a text editor and set your inputs randomization "rules" and FILL_INPUTS
+
+
+## 2. bf_height.py
+Activate TMI's bruteforce 
+
+Open the script and change the first line TIME_MIN with the time at which the car's height will be evaluated
+
+
+## 3. bf_nosedown.py
+Activate TMI's bruteforce 
+
+Open the script and change the first line TIME_MIN with the time at which the car's nose position will be evaluated
+
+
+## 4. bf_speed.py
+Activate TMI's bruteforce 
+
+Open the script and change the first line TIME_MIN with the time at which the car's speed will be evaluated
+
+
+## 5. bf_point.py
+Activate TMI's bruteforce 
+
+Open the script and change the first line TIME_MIN with the time at which the car's distance to a point will be evaluated
+Also change POINT, the x,y,z coordinates of the point to get close to
---
+++ common_scripts/bf_nosedown.py
@@ -0,0 +1,67 @@
+TIME_MIN = 32500
+TIME_MAX = TIME_MIN
+
+import math
+from tminterface.structs import BFEvaluationDecision, BFEvaluationInfo, BFEvaluationResponse, BFPhase
+from tminterface.interface import TMInterface
+from tminterface.client import Client, run_client
+
+import sys
+
+class MainClient(Client):
+    def __init__(self) -> None:
+        self.best = -1
+        self.time = -1
+
+    def on_registered(self, iface: TMInterface) -> None:
+        print(f'Registered to {iface.server_name}')
+
+    def on_simulation_begin(self, iface):
+        self.lowest_time = iface.get_event_buffer().events_duration
+        print(f"Base run time: {self.lowest_time}")
+        if not (TIME_MIN <= TIME_MAX <= self.lowest_time):
+            print("ERROR: MUST HAVE 'TIME_MIN <= TIME_MAX <= REPLAY_TIME'")
+
+    def on_bruteforce_evaluate(self, iface, info: BFEvaluationInfo) -> BFEvaluationResponse:
+        self.current_time = info.time
+        self.phase = info.phase
+
+        response = BFEvaluationResponse()
+        response.decision = BFEvaluationDecision.DO_NOTHING
+        
+        if self.phase == BFPhase.INITIAL:
+            if self.is_eval_time() and self.is_better(iface):
+                self.best = self.current
+                self.time = self.current_time
+
+            if self.is_past_eval_time():
+                print(f"base at {self.time}: {self.best=}")
+
+        elif self.phase == BFPhase.SEARCH:
+            if self.is_eval_time() and self.is_better(iface):
+                response.decision = BFEvaluationDecision.ACCEPT
+                        
+            if self.is_past_eval_time():
+                if response.decision != BFEvaluationDecision.ACCEPT:
+                    response.decision = BFEvaluationDecision.REJECT
+
+        return response
+
+    def is_better(self, iface):
+        """Evaluates if the iteration is better when parameter == Optimize.CUSTOM"""
+        self.current = abs((math.pi / 2) - iface.get_simulation_state().yaw_pitch_roll[1])
+        return self.best == -1 or self.current < self.best
+
+    def is_eval_time(self):
+        return TIME_MIN <= self.current_time <= TIME_MAX
+
+    def is_past_eval_time(self):
+        return TIME_MAX <= self.current_time
+
+def main():
+    server_name = f'TMInterface{sys.argv[1]}' if len(sys.argv) > 1 else 'TMInterface0'
+    print(f'Connecting to {server_name}...')
+    run_client(MainClient(), server_name)
+
+if __name__ == '__main__':
+    main()
