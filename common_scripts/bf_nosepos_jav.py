cc4aef1e91adfd268930090c3db8643f34a71015
Romain Lambert <naruto92250@gmail.com>
2022-02-20 02:19:46 +0100

    bf_nosepos_jav common_scripts

---
+++ common_scripts/bf_nosepos_jav.py
@@ -0,0 +1,63 @@
+TIME_MIN = 32500
+
+import math
+from tminterface.structs import BFEvaluationDecision, BFEvaluationInfo, BFEvaluationResponse, BFPhase
+from tminterface.interface import TMInterface
+from tminterface.client import Client, run_client
+
+import sys
+
+class MainClient(Client):
+    def __init__(self) -> None:
+        self.best = -1
+        self.time = -1
+
+    def on_registered(self, iface: TMInterface) -> None:
+        print(f'Registered to {iface.server_name}')
+
+    def on_simulation_begin(self, iface):
+        self.lowest_time = iface.get_event_buffer().events_duration
+        print(f"Base run time: {self.lowest_time}")
+        if not (TIME_MIN <= self.lowest_time):
+            print("ERROR: MUST HAVE 'TIME_MIN <= REPLAY_TIME'")
+
+    def on_bruteforce_evaluate(self, iface, info: BFEvaluationInfo) -> BFEvaluationResponse:
+        response = BFEvaluationResponse()
+        response.decision = BFEvaluationDecision.DO_NOTHING
+
+        if TIME_MIN == info.time:
+            if self.is_better(iface):
+                response.decision = BFEvaluationDecision.ACCEPT
+                self.best = self.current
+                print(f"base at {TIME_MIN}: {self.best=}")
+            else:
+                response.decision = BFEvaluationDecision.REJECT
+
+        return response
+
+    def is_better(self, iface):
+        state = iface.get_simulation_state()
+
+        car_yaw, car_pitch, car_roll = state.yaw_pitch_roll
+
+        target_yaw = math.atan2(state.velocity[0], state.velocity[2])
+        target_pitch = to_rad(90)
+        target_roll = to_rad(0)
+
+        self.current = to_deg(abs(car_yaw - target_yaw) + abs(car_pitch - target_pitch) + abs(car_roll - target_roll))
+
+        return self.best == -1 or self.current < self.best
+
+def to_rad(deg):
+    return deg / 180 * math.pi
+
+def to_deg(rad):
+    return rad * 180 / math.pi
+
+def main():
+    server_name = f'TMInterface{sys.argv[1]}' if len(sys.argv) > 1 else 'TMInterface0'
+    print(f'Connecting to {server_name}...')
+    run_client(MainClient(), server_name)
+
+if __name__ == '__main__':
+    main()
